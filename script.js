a1510373c90433118aa888fc9777120d6cd7f856
vdskn-online <veerappakambli@gmail.com>
2026-08-29 16:30:37 +0530

    Add functions for registration, login, and menu alerts

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
+function newRegistration() {
 
+    alert("ಹೊಸ ನೋಂದಣಿ ಪುಟ ಶೀಘ್ರದಲ್ಲೇ ತೆರೆಯುತ್ತದೆ.");
+
+}
+
+
+function login() {
+
+    alert("ಲಾಗಿನ್ ಪುಟ ಶೀಘ್ರದಲ್ಲೇ ತೆರೆಯುತ್ತದೆ.");
+
+}
+
+
+function callUs() {
+
+    window.location.href = "tel:8095713511";
+
+}
+
+
+function openMenu() {
+
+    alert("Menu ಶೀಘ್ರದಲ್ಲೇ ತೆರೆಯುತ್ತದೆ.");
+
+}
